2ccbb15e348c5fc3b9ea16341be59935d9d3d10b
kod1ngclub <wiiwefwefbi@gmail.com>
2025-03-23 21:49:10 +0900

    FEAT: Add 'uid' parameter at method of view (for one-way sync from model to viewmodel)

--- src/view/note.ts
+++ src/view/note.ts
@@ -1,7 +1,7 @@
 import { ViewNoteUid, ViewNoteContent } from "./model/note"
 
 export interface NoteView {
-    AppendNote(content: ViewNoteContent): void
+    AppendNote(uid: ViewNoteUid, content: ViewNoteContent): void
     UpdateNote(uid: ViewNoteUid, content: ViewNoteContent): void
     DeleteNote(uid: ViewNoteUid): void
 }
